b2f170fb1dd976457d27cf3b6e0e4ecb3232dfbf
putter-kamolprabha <c27thinnaphat.kamolprabha@afacademy.af.edu>
2025-02-12 22:40:00 -0700

    Submit

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -50,22 +50,22 @@ begin
 	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
        w_addends <= x"28"; w_Cin <= '1'; wait for 10 ns; --Case 1 
-	       assert (w_sum = x"A" and w_Cout = '0') report "bad with 28+1" severity failure; 
+	       assert (w_sum = x"B" and w_Cout = '0') report "bad with 28+1" severity failure; 
 	       
 	   w_addends <= x"FA"; w_Cin <= '1'; wait for 10 ns; --Case 2  
-	       assert (w_sum = x"FB" and w_Cout = '1') report "bad with FA+1 " severity failure; 
+	       assert (w_sum = x"A" and w_Cout = '1') report "bad with FA+1 " severity failure; 
 	       
 	   w_addends <= x"57"; w_Cin <= '1'; wait for 10 ns; --Case 3 
-	       assert (w_sum = x"58" and w_Cout = '1') report "bad with 57+1 " severity failure; 
+	       assert (w_sum = x"D" and w_Cout = '0') report "bad with 57+1 " severity failure; 
 	       
 	   w_addends <= x"05"; w_Cin <= '0'; wait for 10 ns; --Case 4 
-	       assert (w_sum = x"05" and w_Cout = '0') report "bad with 05+0 " severity failure; 
+	       assert (w_sum = x"5" and w_Cout = '0') report "bad with 05+0 " severity failure; 
 	   
 	   w_addends <= x"87"; w_Cin <= '1'; wait for 10 ns; --Case 5 
-	       assert (w_sum = x"88" and w_Cout = '1') report "bad with 87+1" severity failure; 
+	       assert (w_sum = x"0" and w_Cout = '1') report "bad with 87+1" severity failure; 
 	       
 	   w_addends <= x"EF"; w_Cin <= '0'; wait for 10 ns; --Case 6 
-	       assert (w_sum = x"EF" and w_Cout = '1') report "bad with EF+0 " severity failure;     
+	       assert (w_sum = x"D" and w_Cout = '1') report "bad with EF+0 " severity failure;     
 
  
        
